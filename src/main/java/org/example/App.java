492facede969fee85cf6cf6f16438f2fd502889e
MileninSergey <milenins@yandex.ru>
2024-06-17 23:19:22 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Bot bot = new Bot("7438264774:AAFCs4lHfPNk2tsb60dxWyL1He74Jq72kJk");
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(BotSession.class);
+
+            telegramBotsApi.registerBot(bot);
+
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/example/Bot.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public Bot(String botToken) {
+        super(botToken);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println(update.getMessage().getText());
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "alina_version1_bot";
+    }
+}
